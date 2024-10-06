fd93cb650bf2058244dc15cf20d97097514fe270
CringeStar <spaylon248@gmail.com>
2024-10-06 19:46:51 +0300

    add chapter03 и chapter04

--- src/chapter03.rs
+++ src/chapter03.rs
@@ -1,13 +1,121 @@
 #[test]
-fn test() {
-    assert_eq!(2 + 3, 5);
-}
-
-#[test]
 fn test1() {
     let x: i32 = 5; // Uninitialized but used, ERROR !
     let _y: i32; // Uninitialized but also unused, only a Warning !
 
     assert_eq!(x, 5);
     println!("Success!");
+}
+
+#[test]
+fn test2() {
+// Fill the blanks in the code to make it compile
+    fn main() {
+        let mut x = 1;
+        x += 2;
+
+        assert_eq!(x, 3);
+        println!("Success!");
+    }
+}
+
+#[test]
+fn test3() {
+
+    // Fix the error below with least amount of modification
+    fn main() {
+        let x: i32 = 10;
+        let y: i32;
+        {
+            y = 5;
+            println!("The value of x is {} and value of y is {}", x, y);
+        }
+        println!("The value of x is {} and value of y is {}", x, y);
+    }
+}
+
+#[test]
+fn test4() {
+    // Fix the error with the use of define_x
+    fn main() {
+        let x = define_x();
+        println!("{}, world", x);
+    }
+
+    fn define_x() -> &'static str{
+        let x = "hello";
+        x
+    }
+}
+
+#[test]
+fn test5() {
+    // Only modify `assert_eq!` to make the `println!` work(print `42` in terminal)
+    fn main() {
+        let x: i32 = 5;
+        {
+            let x = 12;
+            assert_eq!(x, 12);
+        }
+
+        assert_eq!(x, 5);
+
+        let x = 42;
+        println!("{}", x); // Prints "42".
+    }
+}
+
+#[test]
+fn test6() {
+    // Remove a line in the code to make it compile
+    fn main() {
+        let mut x: i32 = 1;
+        x = 7;
+        // Shadowing and re-binding
+        let x = x;
+
+
+        let y = 4;
+        // Shadowing
+        let y = "I can also be bound to text!";
+
+        println!("Success!");
+    }
+}
+
+#[test]
+fn test7() {
+    fn main() {
+        let x = 1;
+        println!("{}", x);
+    }
+
+    // Warning: unused variable: x
+}
+
+#[test]
+fn test8() {
+    // Fix the error below with least amount of modification
+    fn main() {
+        let (x, y) = (1, 2);
+        let x = x + 2;
+
+        assert_eq!(x, 3);
+        assert_eq!(y, 2);
+
+        println!("Success!");
+    }
+}
+
+#[test]
+fn test9() {
+    fn main() {
+        let (x, y);
+        (x,..) = (3, 4);
+        [.., y] = [1, 2];
+        // Fill the blank to make the code work
+        assert_eq!([x,y], [3,2]);
+
+        println!("Success!");
+    }
 }
---
+++ src/chapter04.rs
@@ -0,0 +1,25 @@
+// chapter 04.1
+#[test]
+fn test1() {
+    // Remove something to make it work
+    fn main() {
+        let x: i32 = 5;
+        let mut y;
+
+        y = x;
+
+        let z = 10; // Type of z ?
+
+        println!("Success!");
+    }
+}
+
+#[test]
+fn test2() {
+    // Fill the blank
+    fn main() {
+        let v: u16 = 38_u8 as u16;
+
+        println!("Success!");
+    }
+}
