2b28070ac32472e098f9813acf2adbfa761c57a0
CringeStar <spaylon248@gmail.com>
2024-10-20 20:40:00 +0300

    add

--- rustByPractice/src/chapter04.rs
+++ rustByPractice/src/chapter04.rs
@@ -52,8 +52,95 @@ fn test4() {
 fn test5() {
     // Fix errors and panics to make it work
 
-    let v1 = 251_u8 + 8;
-    let v2 = i8::checked_add(251, 8).unwrap();
+    let v1 = 251_i32 + 8;
+    let v2 = i32::checked_add(251, 8).unwrap();
     println!("{},{}", v1, v2);
 }
 
+#[test]
+fn test6() {
+    // Modify `assert!` to make it work
+    let v = 1_024 + 0xff + 0o77 + 0b1111_1111;
+    assert!(v == 1597);
+
+    println!("Success!");
+}
+
+#[test]
+fn test7() {
+    // Fill the blank to make it work
+
+    let x = 1_000.000_1; // ?
+    let y: f32 = 0.12; // f32
+    let z = 0.01_f64; // f64
+
+    assert_eq!(type_of(&x), "f64".to_string());
+    println!("Success!");
+
+    fn type_of<T>(_: &T) -> String {
+        format!("{}", std::any::type_name::<T>())
+    }
+}
+
+#[test]
+fn test8() {
+    fn main() {
+        assert!(0.1 + 0.2 == 0.4 - 0.1);
+
+        println!("Success!");
+    }
+}
+
+#[test]
+fn test9() {
+    let mut sum = 0;
+    for i in -3..2 {
+        sum += i
+    }
+
+    assert!(sum == -5);
+
+    for c in 'a'..='z' {
+        println!("{}", c as u32);
+    }
+}
+
+#[test]
+fn test10() {
+    // Fill the blanks
+    use std::ops::{Range, RangeInclusive};
+
+    assert_eq!((1..5), Range { start: 1, end: 5 });
+    assert_eq!((1..=5), RangeInclusive::new(1, 5));
+
+    println!("Success!");
+}
+
+#[test]
+fn test11() {
+
+    // Fill the blanks and fix the errors
+    // Integer addition
+    assert!(1u32 + 2 == 3);
+
+    // Integer subtraction
+    assert!(1i32 - 2 == -1);
+    //assert!(1u8 - 2 == -1);
+
+    assert!(3 * 50 == 150);
+
+    assert!(9.6 / 3.2 == 3.0); // error ! make it work
+
+    assert!(24 % 5 == 1);
+    // Short-circuiting boolean logic
+    assert!(true && false == true);
+    assert!(true || false == true);
+    assert!(!true == true);
+
+    // Bitwise operations
+    println!("0011 AND 0101 is {:04b}", 0b0011u32 & 0b0101);
+    println!("0011 OR 0101 is {:04b}", 0b0011u32 | 0b0101);
+    println!("0011 XOR 0101 is {:04b}", 0b0011u32 ^ 0b0101);
+    println!("1 << 5 is {}", 1u32 << 5);
+    println!("0x80 >> 2 is 0x{:x}", 0x80u32 >> 2);
+}
