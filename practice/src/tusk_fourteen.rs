9ed28dff25be4b3dfc1ca1bdc13567895ea17820
Коломиец Даниил <CringeStarr>
2024-11-03 19:30:20 +0200

    add

--- practice/src/tusk_fourteen.rs
+++ practice/src/tusk_fourteen.rs
@@ -1,61 +1,47 @@
-//Вибачте за такий код(  Я навіть такий робив увесь день
-
-
-//Точка на площині
 struct Point {
     x: i32,
     y: i32,
 }
 
-//Прямокутник на площині
 struct Rectangle {
     a: Point,
     b: Point,
 }
 
-
-//Рахує зайняту площу
 fn area_occupied(xs: &Vec<Rectangle>) -> i32 {
-    let mut temp_one = 0;
-    let mut temp_two = 0;
-    let mut result = 0;
-
-    let mut sum = xs.iter().map(|x| {
+    let sum: i32 = xs.iter().map(|x| {
         let height = if x.a.y < x.b.y { x.b.y - x.a.y } else { x.a.y - x.b.y };
         let width = if x.a.x < x.b.x { x.b.x - x.a.x } else { x.a.x - x.b.x };
         height * width
     }).sum();
 
+    fn delete_common_area(rec_one: &Rectangle, rec_two: &Rectangle) -> i32 {
+        let mut common_width = 0;
+        let mut common_height = 0;
+        let result;
 
-    for i in 0..xs[0].b.x as usize {
-        if i >= xs[0].a.x as usize && i < xs[1].b.x as usize {
-            temp_one += 1;
+        for i in 0..rec_one.b.x as usize {
+            if i >= rec_one.a.x as usize && i < rec_two.b.x as usize {
+                common_width += 1;
+            }
         }
+
+        for i in 0..rec_one.a.y as usize {
+            if i >= rec_one.b.y as usize && i >= rec_two.b.y as usize && i < rec_two.a.y as usize {
+                common_height += 1;
+            }
+        }
+
+        result = common_width * common_height;
+
+        result
     }
 
-    temp_two = (xs[1].a.y + xs[1].b.y) - (xs[0].a.y + xs[0].b.y);
+    let overlap_area_1_2 = delete_common_area(&xs[0], &xs[1]);
+    let overlap_area_1_3 = delete_common_area(&xs[2], &xs[1]);
 
-    result = temp_one * temp_two;
-
-
-    temp_one = 0;
-    temp_two = 0;
-
-    for i in 0..xs[2].b.x as usize {
-        if i >= xs[2].a.x as usize && i < xs[1].b.x as usize {
-            temp_one += 1;
-        }
-    }
-
-    temp_two = (xs[1].a.y + xs[1].b.y) - (xs[2].a.y + xs[2].b.y);
-
-    result += temp_one * temp_two;
-
-    sum -= result;
-
-    sum
+    sum - (overlap_area_1_2 + overlap_area_1_3)
 }
-
 
 fn test_data() -> Vec<Rectangle> {
     vec![
